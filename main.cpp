c05859c2c75f6282f060811b0cfe8703ff1e1760
Eurry <92621710+PyFider@users.noreply.github.com>
2022-05-08 19:39:42 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    while(true) {
+        double bf, bn, q;
+        cout << "Your first number| ";
+        cin >> bf;
+        
+        cout << "Your last number| ";
+        cin >> bn;
+
+        cout << "Your q number| ";
+        cin >> q;
+
+        if (q == 0 or q == 1) {
+            cout << "Answer| q must be != 0 or 1";
+        }
+
+        if (q != 0 and q != 1) {
+            double s = (bn*q-bf)/(q-1);
+            cout << s;
+        }
+    }
+}
